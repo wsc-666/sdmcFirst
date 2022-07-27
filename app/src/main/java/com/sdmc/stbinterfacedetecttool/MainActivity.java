504fc61da018eeb60c6effad26d34f409adb13b1
WuJing <1916888365@qq.com>
2022-07-27 20:19:38 +0800

    111

---
+++ app/src/main/java/com/sdmc/stbinterfacedetecttool/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sdmc.stbinterfacedetecttool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //aaa
+        System.out.println("sdgd");
+        //fdfsfs
+        //54646
+    }
+}
