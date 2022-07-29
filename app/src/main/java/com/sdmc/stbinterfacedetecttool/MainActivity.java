26cff7428d6cca6e68cb05b04e0c6da77920564d
GaryChenHao <765104903@qq.com>
2022-07-29 10:42:47 +0800

    第一次修改

--- app/src/main/java/com/sdmc/stbinterfacedetecttool/MainActivity.java
+++ app/src/main/java/com/sdmc/stbinterfacedetecttool/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //4891819819iashdiuashduaishduiduiU89AC不i啊是对啊厚度爱好的话，mlm
     }
 }
